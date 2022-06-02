b09c6f9adfac19ebe6c84652471b915516710c1f
Venkatesh Natarajan <vnatarajan@bbex.com>
2022-06-01 23:46:12 -0600

    profile Edit feature

--- tweetproject/tweet_site/tweet_site/settings.py
+++ tweetproject/tweet_site/tweet_site/settings.py
@@ -39,8 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'feed',
-    'accounts',
+    'feed.apps.FeedConfig',
+    'accounts.apps.AccountsConfig',
 ]
 
 MIDDLEWARE = [
